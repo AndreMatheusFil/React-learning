8597afc11ec3c2557162082102330b5c3e70203f
André M. Fil <andrematheusfil@gmail.com>
2023-02-13 00:24:04 -0300

    Secret Word Finalizado

--- 6_secret_word/secret_word/src/components/GameOver.js
+++ 6_secret_word/secret_word/src/components/GameOver.js
@@ -1,12 +1,16 @@
-import './GameOver.css';
-import React from 'react';
+import "./GameOver.css";
+import React from "react";
 
-function GameOver() {
+const GameOver = ({ retry, score }) => {
   return (
-    <div>
-
+    <div className="gameover">
+      <h1>Fim de jogo!</h1>
+      <h2>
+        A sua pontuação foi: <span>{score}</span>!
+      </h2>
+      <button onClick={retry}>Reiniciar</button>
     </div>
-  )
-}
+  );
+};
 
-export default GameOver
+export default GameOver;
--- 6_secret_word/secret_word/src/components/StartScreen.js
+++ 6_secret_word/secret_word/src/components/StartScreen.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import './StartScreen.css';
+import "./StartScreen.css";
+import React from "react";
 
-function StartScreen() {
+const GameStart = ({ startGame }) => {
   return (
-    <div className='start'>
-        <h1>Secret Word</h1>
-        <p>Clique no botão abaixo para começar a jogar</p>
-        <button>Começar o jogo</button>
+    <div className="start">
+      <h1>Secret Word</h1>
+      <p>Clique no botão abaixo para começar a jogar</p>
+      <button onClick={startGame}>Começar jogo</button>
     </div>
-  )
-}
+  );
+};
 
-export default StartScreen
+export default GameStart;
